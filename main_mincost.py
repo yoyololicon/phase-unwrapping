5e76b77794e28bbf19040afc80a8b37d929e0318
joey_yu <Joey_Yu@htc.com>
2020-06-12 15:11:08 +0800

    add sparse data version

--- main_mincost.py
+++ main_mincost.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 from skimage import data, img_as_float, color, exposure
 from skimage.restoration import unwrap_phase
 from utils import wrap_func
-from minimum_cost import minimum_cost_PU_grid
+from minimum_cost import minimum_cost_PU_grid, minimum_cost_PU_sparse
 
 shape = (128,) * 2
-height = 13 * np.pi
+height = 6 * np.pi
 mean = np.array(shape) * 0.5
 cov = np.array([15 ** 2, 15 ** 2, ])
 rv = multivariate_normal(mean, cov)
@@ -16,8 +16,8 @@ rv = multivariate_normal(mean, cov)
 x, y = np.meshgrid(np.arange(shape[0]), np.arange(shape[1]))
 output = rv.pdf(np.dstack((x, y)))
 output *= height / output.max()
-output[:64, :64] = 0
-output += np.random.randn(*output.shape) * 0.6
+#output[:64, :64] = 0
+output += np.random.randn(*output.shape) * 0.8
 psi = wrap_func(output)
 plt.subplot(211)
 plt.imshow(output)
@@ -31,17 +31,36 @@ image = color.rgb2gray(img_as_float(data.chelsea()))
 image = exposure.rescale_intensity(image, out_range=(0, 4 * np.pi))
 # Create a phase-wrapped image in the interval [-pi, pi)
 psi = np.angle(np.exp(1j * image))
+#image = output
+
+x, y = np.meshgrid(np.arange(psi.shape[1]), np.arange(psi.shape[0]))
+sparsity = 0.5
+tmp = np.vstack((x.ravel(), y.ravel(), psi.ravel(), image.ravel())).T
+np.random.shuffle(tmp)
+sp_x, sp_y, sp_psi, sp_image = tmp[:int(tmp.shape[0] * sparsity)].T
 
 plt.subplot(211)
-plt.imshow(image, cmap='gray')
+#plt.imshow(image, cmap='gray')
+plt.scatter(sp_x, -sp_y, c=sp_image, cmap='gray', s=0.05)
 plt.subplot(212)
-plt.imshow(psi, cmap='gray')
+#plt.imshow(psi, cmap='gray')
+plt.scatter(sp_x, -sp_y, c=sp_psi, cmap='gray', s=0.05)
 plt.show()
 
 
-plt.subplot(211)
-plt.imshow(unwrap_phase(psi), cmap='gray')
-
-plt.subplot(212)
-plt.imshow(minimum_cost_PU_grid(psi), cmap='gray')
+fig = plt.figure(figsize=(8, 12))
+plt.subplot(311)
+#plt.imshow(unwrap_phase(psi), cmap='gray')
+plt.scatter(sp_x, -sp_y, c=sp_image, cmap='gray', s=0.05)
+plt.xlim(0, sp_x.max() - 1)
+plt.ylim(-sp_y.max() + 1, 0)
+plt.subplot(312)
+plt.scatter(sp_x, -sp_y, c=sp_psi, cmap='gray', s=0.05)
+plt.xlim(0, sp_x.max() - 1)
+plt.ylim(-sp_y.max() + 1, 0)
+plt.subplot(313)
+#plt.imshow(minimum_cost_PU_sparse(x.ravel(), y.ravel(), psi.ravel()).reshape(*psi.shape), cmap='gray')
+plt.scatter(sp_x, -sp_y, c=minimum_cost_PU_sparse(sp_x, sp_y, sp_psi, capacity=1), cmap='gray', s=0.05)
+plt.xlim(0, sp_x.max() - 1)
+plt.ylim(-sp_y.max() + 1, 0)
 plt.show()
--- minimum_cost.py
+++ minimum_cost.py
@@ -1,6 +1,8 @@
 import numpy as np
 import networkx as nx
+from scipy.spatial import Delaunay
 from utils import wrap_func as W
+import matplotlib.pyplot as plt
 
 
 def minimum_cost_PU_grid(x: np.ndarray, capacity=None):
@@ -10,7 +12,7 @@ def minimum_cost_PU_grid(x: np.ndarray, capacity=None):
     if capacity is None:
         cap_args = dict()
     else:
-        cap_args = {'capacity', capacity}
+        cap_args = {'capacity': capacity}
 
     psi1 = W(np.diff(x, axis=0))
     psi2 = W(np.diff(x, axis=1))
@@ -18,7 +20,8 @@ def minimum_cost_PU_grid(x: np.ndarray, capacity=None):
     G = nx.Graph()
 
     demands = np.round(-(psi1[:, 1:] - psi1[:, :-1] - psi2[1:, :] + psi2[:-1, :]) * 0.5 / np.pi).astype(np.int)
-    index = np.arange(N * M).reshape(N, M)[:-1, :-1]
+    index = np.arange(N * M).reshape(N, M)
+    demands = np.pad(demands, ((0, 1),) * 2, 'constant', constant_values=0)
 
     G.add_nodes_from(zip(index.ravel(), [{'demand': d} for d in demands.ravel()]))
     G.add_node(-1, demand=-demands.sum())
@@ -27,36 +30,30 @@ def minimum_cost_PU_grid(x: np.ndarray, capacity=None):
         np.vstack((index[:, :-1].ravel(), index[:, 1:].ravel())).T,
         np.vstack((index[:-1].ravel(), index[1:].ravel())).T)
     )
-    weights = np.concatenate(
-        (((demands[:, :-1] == 0)).ravel(),
-         ((demands[:-1] == 0)).ravel())
-    ).astype(np.int).tolist()
+    weights = np.concatenate(((demands[:, :-1] == 0).ravel(), (demands[:-1] == 0).ravel()))
+    print(weights.sum())
 
     G.add_weighted_edges_from(zip(edges[:, 0], edges[:, 1], weights), **cap_args)
 
-    G.add_weighted_edges_from(zip([-1] * (M - 1),
-                                  range(M - 1),
-                                  (demands[0] == 0).astype(np.int).tolist())
-                              , **cap_args)
-    G.add_weighted_edges_from(
-        zip([-1] * (M - 1),
-            range((N - 2) * M, (N - 1) * M - 1),
-            (demands[-1] == 0).astype(np.int).tolist()),
+    G.add_edges_from(zip([-1] * M, range(M))
+                     , **cap_args)
+    G.add_edges_from(
+        zip([-1] * M,
+            range((N - 1) * M, (N - 0) * M - 1)),
         **cap_args)
-    G.add_weighted_edges_from(
-        zip([-1] * (N - 1),
-            range(0, (N - 1) * M, M),
-            (demands[:, 0] == 0).astype(np.int).tolist()),
+    G.add_edges_from(
+        zip([-1] * (N - 0),
+            range(0, (N - 0) * M, M)),
         **cap_args)
-    G.add_weighted_edges_from(
-        zip([-1] * (N - 1),
-            range(M - 2, (N - 1) * M, M),
-            (demands[:, -1] == 0).astype(np.int).tolist()),
+    G.add_edges_from(
+        zip([-1] * (N - 0),
+            range(M - 1, (N - 0) * M, M), ),
         **cap_args)
 
     G = G.to_directed()
 
     cost, flowdict = nx.network_simplex(G)
+    print(cost)
 
     K2 = np.empty((N, M - 1))
     K1 = np.empty((N - 1, M))
@@ -64,22 +61,18 @@ def minimum_cost_PU_grid(x: np.ndarray, capacity=None):
     for i in range(N - 1):
         for j in range(M):
             if j == 0:
-                K1[i][0] = flowdict[-1][i * M] - flowdict[i * M][-1]
-            elif j == M - 1:
-                K1[i][M - 1] = flowdict[i * M + j - 1][-1] - flowdict[-1][i * M + j - 1]
+                K1[i][0] = -flowdict[-1][i * M] + flowdict[i * M][-1]
             else:
-                K1[i][j] = flowdict[i * M + j - 1][i * M + j] - flowdict[i * M + j][i * M + j - 1]
+                K1[i][j] = -flowdict[i * M + j - 1][i * M + j] + flowdict[i * M + j][i * M + j - 1]
 
     for i in range(N):
         for j in range(M - 1):
             if i == 0:
                 K2[i][j] = flowdict[-1][j] - flowdict[j][-1]
-            elif i == N - 1:
-                K2[i][j] = flowdict[(i - 1) * M + j][-1] - flowdict[-1][(i - 1) * M + j]
             else:
                 K2[i][j] = flowdict[(i - 1) * M + j][i * M + j] - flowdict[i * M + j][(i - 1) * M + j]
 
-    K2[0, 0] = K2[0, -1] = K2[-1, 0] = K2[-1, -1] = 0
+    K2[0, 0] = 0
 
     psi1 += K1 * 2 * np.pi
     psi2 += K2 * 2 * np.pi
@@ -88,3 +81,67 @@ def minimum_cost_PU_grid(x: np.ndarray, capacity=None):
     y[1:, 0] = np.cumsum(psi1[:, 0])
     y[:, 1:] = np.cumsum(psi2, axis=1) + y[:, :1]
     return y
+
+
+def minimum_cost_PU_sparse(x, y, psi, capacity=None):
+    points = np.vstack((x, y)).T
+    num_points = points.shape[0]
+    tri = Delaunay(points)
+    simplex = tri.simplices
+    simplex_neighbors = tri.neighbors
+    num_simplex = simplex.shape[0]
+
+    if capacity is None:
+        cap_args = dict()
+    else:
+        cap_args = {'capacity': capacity}
+
+    psi_diff = W(psi[simplex] - psi[np.roll(simplex, 1, 1)])
+    edges = np.stack((np.roll(simplex, 1, 1), simplex), axis=2).reshape(-1, 2)
+    simplex_edges = np.stack((
+        np.broadcast_to(np.arange(num_simplex)[:, None], simplex_neighbors.shape),
+        np.roll(simplex_neighbors, -1, 1)), axis=2
+    ).reshape(-1, 2)
+    edges, unique_idx = np.unique(edges, axis=0, return_index=True)
+    simplex_edges = simplex_edges[unique_idx]
+
+    demands = np.round(-psi_diff.sum(1) * 0.5 / np.pi).astype(np.int)
+    psi_diff = psi_diff.flatten()[unique_idx]
+
+    G = nx.Graph()
+
+    traverse_G = nx.Graph()
+    traverse_G.add_edges_from(edges)
+    traverse_path = list(nx.algorithms.traversal.breadth_first_search.bfs_edges(traverse_G, 0))
+
+    G.add_nodes_from(zip(range(num_simplex),
+                         [{'demand': d} for d in demands]))
+    G.add_node(-1, demand=-demands.sum())
+    print(demands.sum())
+
+    weights = np.sum(demands[simplex_edges] == 0, 1)
+    G.add_weighted_edges_from(zip(simplex_edges[:, 0], simplex_edges[:, 1], weights), **cap_args)
+
+    G = G.to_directed()
+    cost, flowdict = nx.network_simplex(G)
+    print(cost)
+
+    K = np.zeros(edges.shape[0])
+
+    for j in range(simplex_edges.shape[0]):
+        u, v = simplex_edges[j]
+        K[j] = -flowdict[u][v] + flowdict[v][u]
+
+    psi_diff += K * 2 * np.pi
+    print(K.max(), K.min(), np.abs(K).sum())
+
+    psi_dict = {i: {} for i in range(num_points)}
+
+    for diff, (u, v) in zip(psi_diff, edges):
+        psi_dict[u][v] = diff
+        psi_dict[v][u] = -diff
+
+    result = psi.copy()
+    for u, v in traverse_path:
+        result[v] = result[u] + psi_dict[u][v]
+    return result
